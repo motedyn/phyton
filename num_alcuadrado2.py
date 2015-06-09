6f3ea1ac94b19eb3312733b354ce6bce8322198c
Motedyn <motedyn@gmail.com>
2015-06-09 15:25:43 -0600

    Create num_alcuadrado2.py

---
+++ num_alcuadrado2.py
@@ -0,0 +1,5 @@
+def potencia(base,exponente):  # Agregá tus parámetros acá.
+    resultado = base**exponente
+    print "%d a la %d potencia es %d." % (base, exponente, resultado)
+
+potencia(5,2)  # Agregá tus parámetros acá.
